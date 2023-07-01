11734bc152b51e8dc7617f390fd1be9b15e68e86
nikhils9 <nikhilshetty447@gmail.com>
2023-07-01 13:58:51 +0530

    Add script to generate credentials

--- hello_world/generate-credentials.ts
+++ hello_world/generate-credentials.ts
@@ -1,5 +1,20 @@
-import { AppWallet } from "npm:meshsdk/core";
+import { AppWallet, resolvePrivateKey } from "npm:@meshsdk/core@1.5.4";
 
+// Generate a menomic phrase to create new wallet subsequently
 const mnemonic = AppWallet.brew();
 
-console.log(mnemonic);
+const privateKey = resolvePrivateKey(mnemonic);
+await Deno.writeTextFile("key.sk", privateKey);
+
+// Create a wallet instance for testnet (networkId == 0)
+// Fun fact: The testnet wallet created will work for both Preview & Preprod. You decide which network you want to use.
+const wallet = new AppWallet({
+    networkId: 0,
+    key: {
+      type: 'mnemonic',
+      words: mnemonic,
+    },
+});
+
+const address = wallet.getPaymentAddress();
+await Deno.writeTextFile("key.addr", address);
